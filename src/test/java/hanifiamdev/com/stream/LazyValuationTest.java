0c0dc18f6c8886555dd5c66d995afcdf1b5d6e43
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-29 16:34:23 +0700

    Lazy Evaluation

---
+++ src/test/java/hanifiamdev/com/stream/LazyValuationTest.java
@@ -0,0 +1,35 @@
+package hanifiamdev.com.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LazyValuationTest {
+
+    @Test
+    void testIntermediateOperation() {
+
+        List<String> names = List.of("Hanif", "Fachri", "Orkhan");
+
+        Stream<String> upper = names.stream()
+                .map(name -> {
+                    System.out.println("Change " + name + " to UPPERCASE");
+                        return name.toUpperCase();
+                });
+    }
+
+    @Test
+    void testTerminalOperation() {
+
+        List<String> names = List.of("Hanif", "Fachri", "Orkhan");
+
+        names.stream()
+                .map(name -> {
+                    System.out.println("Change " + name + " to UPPERCASE");
+                    return name.toUpperCase();
+                }).forEach(upper -> {
+                    System.out.println(upper);
+                });
+    }
+}
